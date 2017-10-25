2f53101773680f2a28b0cd99331e50a729537638
Jesper Sindahl Nielsen <jasn@cs.au.dk>
2017-10-25 16:43:17 +0200

    single underscores

--- regnskaber/ioqueue.py
+++ regnskaber/ioqueue.py
@@ -5,77 +5,77 @@ from queue import Empty
 import sys
 
 class IOQueue:
-    __sentinel = "$ioqueue_sentinel$"
-    __buffer_size = 128  # elements.
+    _sentinel = "$ioqueue_sentinel$"
+    _buffer_size = 128  # elements.
 
     def __init__(self, filename):
-        self.__push_buffer = []
-        self.__pop_buffer = []
-        self.__buffer_idx = 0
-        self.__popped = 0
-        self.__pushed = 0
-        self.__filename = filename
-        self.__seek_to = 0
+        self._push_buffer = []
+        self._pop_buffer = []
+        self._buffer_idx = 0
+        self._popped = 0
+        self._pushed = 0
+        self._filename = filename
+        self._seek_to = 0
 
     def get_statistics(self):
-        return self.__popped, self.__pushed
+        return self._popped, self._pushed
 
     def size(self):
-        return self.__pushed - self.__popped
+        return self._pushed - self._popped
 
     def get(self):
-        if self.__buffer_idx >= len(self.__pop_buffer):
-            self.__fill_pop_buffer()
-        if len(self.__pop_buffer) > self.__buffer_idx:
-            self.__buffer_idx += 1
-            self.__popped += 1
-            return self.__pop_buffer[self.__buffer_idx - 1]
+        if self._buffer_idx >= len(self._pop_buffer):
+            self._fill_pop_buffer()
+        if len(self._pop_buffer) > self._buffer_idx:
+            self._buffer_idx += 1
+            self._popped += 1
+            return self._pop_buffer[self._buffer_idx - 1]
         raise Empty
 
     def put(self, element):
-        if len(self.__push_buffer) >= IOQueue.__buffer_size:
-            self.__flush_buffer()
-        assert(len(self.__push_buffer) < IOQueue.__buffer_size)
-        self.__push_buffer.append(element)
-        self.__pushed += 1
+        if len(self._push_buffer) >= IOQueue._buffer_size:
+            self._flush_buffer()
+        assert(len(self._push_buffer) < IOQueue._buffer_size)
+        self._push_buffer.append(element)
+        self._pushed += 1
 
-    def __fill_pop_buffer(self):
-        assert(self.__buffer_idx >= len(self.__pop_buffer))
-        if self.size() < IOQueue.__buffer_size:
-            self.__pop_buffer = [x for x in self.__push_buffer]
-            self.__push_buffer = []
-            self.__buffer_idx = 0
+    def _fill_pop_buffer(self):
+        assert(self._buffer_idx >= len(self._pop_buffer))
+        if self.size() < IOQueue._buffer_size:
+            self._pop_buffer = [x for x in self._push_buffer]
+            self._push_buffer = []
+            self._buffer_idx = 0
             return
 
-        with open(self.__filename) as qfile:
-            qfile.seek(self.__seek_to)
+        with open(self._filename) as qfile:
+            qfile.seek(self._seek_to)
             contents = ""
             while True:
                 contents += qfile.read(2**16)
-                if contents.find(IOQueue.__sentinel):
+                if contents.find(IOQueue._sentinel):
                     break
 
-            idx = contents.find(IOQueue.__sentinel)
-            self.__seek_to += idx + len(IOQueue.__sentinel)
+            idx = contents.find(IOQueue._sentinel)
+            self._seek_to += idx + len(IOQueue._sentinel)
             contents = contents[:idx]
 
             try:
-                self.__pop_buffer = [x for x in json.loads(contents)]
+                self._pop_buffer = [x for x in json.loads(contents)]
             except json.JSONDecodeError as e:
                 print(contents, file=sys.stderr)
                 raise
-            self.__pop_buffer = [tuple(x) if type(x) == list else x
-                                 for x in self.__pop_buffer]
-            self.__buffer_idx = 0
+            self._pop_buffer = [tuple(x) if type(x) == list else x
+                                 for x in self._pop_buffer]
+            self._buffer_idx = 0
             return
 
-    def __flush_buffer(self):
-        assert(len(self.__push_buffer) > 0)
-        with open(self.__filename, 'a') as qfile:
-            to_write = json.dumps(self.__push_buffer)
+    def _flush_buffer(self):
+        assert(len(self._push_buffer) > 0)
+        with open(self._filename, 'a') as qfile:
+            to_write = json.dumps(self._push_buffer)
             print(to_write, file=qfile, end='')
-            print(IOQueue.__sentinel, file=qfile, end='')
-            self.__push_buffer = []
+            print(IOQueue._sentinel, file=qfile, end='')
+            self._push_buffer = []
 
 
 class IOQueueManager(managers.BaseManager):
